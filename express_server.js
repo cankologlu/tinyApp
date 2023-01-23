ca4859ef10e7a074f4816ed95f5c288afc86a62e
Can Kologlu <mckologlu@gmail.com>
2023-01-23 14:26:22 -0500

    adding json string our object

--- express_server.js
+++ express_server.js
@@ -3,13 +3,19 @@ const app = express();              // Calling express
 const PORT = 8080;
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",     
+  "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://google.com"
 };
 
 app.get("/", (req, res) => {
   res.send("Hello");
 });
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+})
+app.get("/catoz", (req, res) => {
+  res.send("catz?");
+})
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`)
+  console.log(`Example app listening on port ${PORT}!`);
 })
