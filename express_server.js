1bddcaeb61715c44e8ce4013c98538d60da4f92e
Can Kologlu <mckologlu@gmail.com>
2023-01-23 18:08:59 -0500

    adding the views folder with file to pass in object

--- express_server.js
+++ express_server.js
@@ -1,7 +1,7 @@
 const express = require("express"); // Importing express library
 const app = express();              // Calling express
 const PORT = 8080;
-app.set("view engine", "ejs");
+app.set("view engine", "ejs");      // Setting ejs as the templating engine
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -11,20 +11,53 @@ const urlDatabase = {
 app.get("/", (req, res) => {
   res.send("<html><body>Welcome to the <i>homepage</i></body></html>\n");
 });
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
-app.get("/catoz", (req, res) => {
-  res.send("catz?");
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
-app.get("/set", (req,res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
-});
-app.get("/fetch", (req,res) => {
-  const a = "<html><body><b>epskenik</b></body></html>";
-  res.send(`a = ${a}`);
-});
+
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// app.get("/hello", (req, res) => {
+//   const templateVars = {greeting: "Hello there!"}
+//   res.render("hello_world", templateVars);
+// });
+
+
+
+// app.get("/catoz", (req, res) => {
+//   res.send("catz?");
+// });
+// app.get("/set", (req,res) => {
+//   const a = 1;
+//   res.send(`a = ${a}`);
+// });
+// app.get("/fetch", (req,res) => {
+//   const a = "<html><body><b>epskenik</b></body></html>";
+//   res.send(`a = ${a}`);
+// });
