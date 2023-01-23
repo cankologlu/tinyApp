c208264d4bd6b5b82394d172ea00f4814cf566d0
Can Kologlu <mckologlu@gmail.com>
2023-01-23 13:47:46 -0500

    server app is set

---
+++ express_server.js
@@ -0,0 +1,15 @@
+const express = require("express"); // Importing express library
+const app = express();              // Calling express
+const PORT = 8080;
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://google.com"
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`)
+})
